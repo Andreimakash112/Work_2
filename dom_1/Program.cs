d4cc96ddf29b9beeb330adc3ba4544e4d6907f7f
Andrei.Makash <andrei.makash@yandex.ru>
2023-10-01 15:33:17 +0300

    коментируем dom_1

---
+++ dom_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int uno (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int number = uno("Введите число");
+if (number <100 && number>=1000)
+{
+   Console.WriteLine("Число не трех значное.Введите трех значное число") ;
+   return;
+}
+ Console.WriteLine($"Введенное число `{number}`") ;
+ int x = number / 10 % 10;
+ Console.WriteLine($"Вторая цифра `{x}`");
